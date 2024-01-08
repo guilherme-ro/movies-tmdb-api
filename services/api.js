fd176040d5c0d1458e4b3effcc369feb09176a3d
Guilherme <guilherme.ramos09@gmail.com>
2024-01-08 12:44:25 -0300

    Update api.js to set TMDB_API_KEY

--- services/api.js
+++ services/api.js
@@ -1,14 +1,15 @@
-//import { apiKey } from "../environment/key.js"
+import { TMDB_API_KEY } from "./api_config.js"
+
 
 async function searchMovieByName(title) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.apiKey}&query=${title}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${title}&language=en-US&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results;
 }
 
 async function getPopularMovies() {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.apiKey}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results;
@@ -17,4 +18,4 @@ async function getPopularMovies() {
 export const API = {
     searchMovieByName,
     getPopularMovies
-  }
+}
