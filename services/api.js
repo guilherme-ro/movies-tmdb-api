de864b87c469f7844ed053b78bd6423416633093
Guilherme <guilherme.ramos09@gmail.com>
2024-01-08 11:51:33 -0300

    Update api.js to work with Netlify's environment variable

--- services/api.js
+++ services/api.js
@@ -1,14 +1,14 @@
 //import { apiKey } from "../environment/key.js"
 
 async function searchMovieByName(title) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.apiKey}&query=${title}&language=en-US&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results;
 }
 
 async function getPopularMovies() {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.apiKey}&language=en-US&page=1`;
     const fetchResponse = await fetch(url);
     const { results } = await fetchResponse.json();
     return results;
